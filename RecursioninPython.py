8dfaa4dce27a61ab14d517186e1d0b696e036095
Vaishnu07 <114077532+Vaishnu07@users.noreply.github.com>
2023-08-21 15:04:18 +0530

    Add files via upload

---
+++ RecursioninPython.py
@@ -0,0 +1,84 @@
+# Recursion ----
+#function calling itself
+# for i in range(6):
+#     print(i)
+#     print(i+1)
+# looping ---- block of statements
+# function ---- named set of intructions
+
+
+
+
+
+
+ # Recursion in function
+# def getusernamepassword (username,password):
+#     if username!= 'athiva':
+#         print("Incorrect Username")
+#         username = input('enter username')
+#         password = input('enter password')
+#         getusernamepassword(username,password)
+#     elif password != 'vaishnu':
+#         print("Incorrect password")
+#         username = input('enter username')
+#         password = input('enter password')
+#         getusernamepassword(username, password)
+#
+# username = input('enter username')
+# password = input('enter password')
+# getusernamepassword(username, password)
+
+
+
+# def fact(n):
+#     if n ==0:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+# print(fact(0))
+# print(fact(1))
+# print(fact(2))
+# print(fact(4))  #4*3*2*1
+
+
+
+ def tellMeNumber(n):
+#     return n
+# print(tellMeNumber(50))
+#
+# abs(5.2)
+# abs(-5)
+# abs(5)
+# ord('a')
+# ord('A')
+
+
+# import math
+# no1 = 34.6
+# no2 = -34.6
+# no3 = 34.5
+# no4 = 34.2
+# print(math.ceil(no1))
+# print(math.ceil(no2))
+# print(math.ceil(no3))
+# print(math.ceil(no4))
+#
+# import math
+# no1 = 34.6
+# no2 = -34.6
+# no3 = 34.5
+# no4 = 34.2
+# print(math.floor(no1))
+# print(math.floor(no2))
+# print(math.floor(no3))
+# print(math.floor(no4))
+#
+# import math
+# no1 = 30
+# no2 = -34.6
+# no3 = 25
+# no4 = 65
+# print(math.sqrt(no1))
+# print(math.sqrt(no2))
+# print(math.sqrt(no3))
+# print(math.sqrt(no4))
